d119632da9c2574eaf60fbc73d44265a75a2df3c
Mal-hoti <68082098+Mal-hoti@users.noreply.github.com>
2021-08-05 15:01:46 +0100

    feat: made a network class

---
+++ NeaProject/NeaProject/network.py
@@ -0,0 +1,26 @@
+import socket
+
+class Network:
+   
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # 'socket.AF_INET' is the internet address family for IPv4  
+        # SOCK_STREAM is the socket type for TCP, protocal that is used to trasnport packets on a network
+        # you can use socket.SOCK_DGRAM for UDP, but it isnt that reliable
+
+        self.hostname = socket.gethostname()  # this gets the hostname 
+        self.ip_address = socket.gethostbyname(self.hostname) # getting the IPv4 address using socket.gethostbyname() method
+
+        self.server = str(self.ip_address)
+        self.port = 5555 # port that is open and free to use
+        self.address = (self.server, self.port)
+        self.id = self.connect()
+    
+    def connect(self):
+        try:
+            self.client.connect(self.address)
+            return self.client.recv(2048).decode()
+        except:
+            pass
+
+n = Network()
